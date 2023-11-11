4512720d37fba5439b33ab5606ec3121913518b6
yujin37 <yj061628@gmail.com>
2023-11-11 12:54:45 +0900

    feat: 주문 메뉴 출력

--- src/main/kotlin/christmas/EventPlanner.kt
+++ src/main/kotlin/christmas/EventPlanner.kt
@@ -1,14 +1,23 @@
 package christmas
 
 class EventPlanner {
-    fun Reservation(){
-        val visitDate = InputView().readDate()
-        val OrderMenu = InputView().readMenu()
+
+    private var visitDate: Int = 0
+    private lateinit var orderMenu: MutableMap<String, Int>
+    fun reservation(): MutableMap<String, Int> {
+        visitDate = InputView().readDate()
+        orderMenu = InputView().readMenu()
         OutputView().readCheck(visitDate)
+        return orderMenu
+    }
+
+    fun orderCheck(){
+        OutputView().orderList(orderMenu)
     }
 
 
     fun Management(){
-        Reservation()
+        reservation()
+        orderCheck()
     }
 }
--- src/main/kotlin/christmas/OutputView.kt
+++ src/main/kotlin/christmas/OutputView.kt
@@ -5,4 +5,11 @@ class OutputView {
     fun readCheck(date: Int) {
         println("12월 " + date.toString() + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!")
     }
+
+    fun orderList(orderMenu: MutableMap<String,Int>){
+        println("<주문 메뉴>")
+        orderMenu.forEach { (key, value) ->
+            println("${key} ${value}개")
+        }
+    }
 }
