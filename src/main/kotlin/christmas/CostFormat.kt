aa6e8f0e5c11fae419b9a373ac207259064fb734
yujin37 <yj061628@gmail.com>
2023-11-11 13:33:50 +0900

    feat: 금액 전달 시 천단위 콤마 문자열로 변경

---
+++ src/main/kotlin/christmas/CostFormat.kt
@@ -0,0 +1,12 @@
+package christmas
+
+import java.text.DecimalFormat
+
+class CostFormat {
+    companion object {
+        fun format(cost: Int): String? {
+            val comma = DecimalFormat("#,###")
+            return comma.format(cost)
+        }
+    }
+}
