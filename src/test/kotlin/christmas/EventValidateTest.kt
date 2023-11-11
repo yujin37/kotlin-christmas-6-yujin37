ad8beeee7bda08bd4a60519e2a24d7c08c7feeb9
yujin37 <yj061628@gmail.com>
2023-11-12 01:22:11 +0900

    test: 유효하지 않는 날짜 테스트

--- src/test/kotlin/christmas/EventValidateTest.kt
+++ src/test/kotlin/christmas/EventValidateTest.kt
@@ -1,9 +1,32 @@
 package christmas
 
-import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
-import camp.nextstep.edu.missionutils.test.NsTest
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class EventValidateTest {
+
+    @Test
+    fun `유효하지 않은 날짜 들어오는 경우 에러 발생`(){
+        val validateLink = EventValidate()
+        // 31이상인 경우
+        assertThrows<IllegalArgumentException> {
+            validateLink.dateValidate("32")
+        }
+        // 0이하인 경우
+        assertThrows<IllegalArgumentException> {
+            validateLink.dateValidate("-1")
+        }
+        // 문자열인 경우
+        assertThrows<IllegalArgumentException> {
+            validateLink.dateValidate("가나")
+        }
+        // 빈 값인 경우
+        assertThrows<IllegalArgumentException> {
+            validateLink.dateValidate("")
+        }
+        // 월일 포함
+        assertThrows<IllegalArgumentException> {
+            validateLink.dateValidate("12월 12일")
+        }
+    }
 }
