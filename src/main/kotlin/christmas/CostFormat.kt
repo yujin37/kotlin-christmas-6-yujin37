7faf397a1bbf8388c2e8034d305050f2ff5ac754
yujin37 <yj061628@gmail.com>
2023-11-12 00:49:27 +0900

    refactor: 천단위 콤마 라이브러리 없이 하도록 변경

--- src/main/kotlin/christmas/CostFormat.kt
+++ src/main/kotlin/christmas/CostFormat.kt
@@ -1,12 +1,9 @@
 package christmas
 
-import java.text.DecimalFormat
-
 class CostFormat {
     companion object {
         fun format(cost: Int): String {
-            val comma = DecimalFormat("#,###")
-            return comma.format(cost)
+            return "%,d".format(cost)
         }
     }
 }
