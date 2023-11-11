deae841eb8a979670ed70a9d8096d916d2ea9573
yujin37 <yj061628@gmail.com>
2023-11-12 01:40:28 +0900

    test: 중복된 메뉴 있는지 체크하는 부분 테스트

--- src/test/kotlin/christmas/EventValidateTest.kt
+++ src/test/kotlin/christmas/EventValidateTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.assertThrows
 class EventValidateTest {
 
     @Test
-    fun `유효하지 않은 날짜 들어오는 경우 에러 발생`(){
+    fun `유효하지 않은 날짜 들어오는 경우 에러 발생`() {
         val validateLink = EventValidate()
         // 31이상인 경우
         assertThrows<IllegalArgumentException> {
@@ -52,6 +52,20 @@ class EventValidateTest {
         // 특수기호만 있는 경우
         assertThrows<IllegalArgumentException> {
             validateLink.validateQuantity(":")
+        }
+    }
+
+    @Test
+    fun `중복된 메뉴 있으면 에러 메시지 발생시키기`() {
+
+        // 시저 샐러드 이미 있는데 또 들어올 경우
+        assertThrows<IllegalArgumentException> {
+            EventValidate().checkMenu("시저샐러드", "1", mutableMapOf("시저샐러드" to 1))
+        }
+
+        // 시저 샐러드와 다른 값도 있는데 시저샐러드 들어오는 경우
+        assertThrows<IllegalArgumentException> {
+            EventValidate().checkMenu("시저샐러드", "1", mutableMapOf("시저샐러드" to 1, "레드와인" to 2))
         }
     }
 }
