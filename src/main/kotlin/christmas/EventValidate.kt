4fa04f46b8c80a02e899d1c16f7ac6cf15611a5d
yujin37 <yj061628@gmail.com>
2023-11-12 00:57:28 +0900

    fix: 예제 테스트에 맞게 돌도록 메시지 수정

--- src/main/kotlin/christmas/EventValidate.kt
+++ src/main/kotlin/christmas/EventValidate.kt
@@ -1,12 +1,12 @@
 package christmas
 
 enum class ValidateError(val message: String) {
-    DATE_INVALIDATE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요"),
+    DATE_INVALIDATE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
     REPEAT_MENU("[ERROR] 중복된 주문입니다. 다시 입력해 주세요."),
     NOT_MATCHING_MENU("[ERROR] 메뉴판에 없는 주문입니다. 다시 입력해 주세요."),
     NOT_QUANTITY_BUY("[ERROR] 주문 수량이 0보다 작습니다. 다시 입력해 주세요."),
-    INVALID_QUANTITY("[ERROR] 주문 형식이 올바르지 않습니다. 다시 입력해 주세요."),
-    INVALID_MENU("[ERROR] 주문 형식이 올바르지 않습니다. 다시 입력해 주세요."),
+    INVALID_QUANTITY("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    INVALID_MENU("[ERROR] 메뉴판에 없는 주문입니다. 다시 입력해 주세요."),
     ONLY_DRINK_ORDER("[ERROR] 음료만 주문할 수 없습니다. 다시 입력해 주세요."),
     LIMIT_MENU_ORDER("[ERROR] 메뉴는 한 번에 최대 20개까지만 주문 가능합니다. 다시 입력해 주세요.")
 }
