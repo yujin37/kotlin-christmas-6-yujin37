8413b728bbdf5a92411f218efa8c8fedce4e0f9a
yujin37 <yj061628@gmail.com>
2023-11-14 03:19:49 +0900

    style: 혜택 내역 가격 테스트 코드 코드 컨벤션 적용

--- src/test/kotlin/christmas/EventsTest.kt
+++ src/test/kotlin/christmas/EventsTest.kt
@@ -8,18 +8,18 @@ import org.junit.jupiter.api.Test
 class EventsTest {
 
     @Test
-    fun `크리스마스 할인 확인`(){
+    fun `크리스마스 할인 확인`() {
         // 둘쨋날 할인 확인
         val result = SpecialEvent().christmasDay(2)
         assertThat(result).isEqualTo(1100)
         // 크리스마스 당일 할인 확인
-        val result2= SpecialEvent().christmasDay(25)
+        val result2 = SpecialEvent().christmasDay(25)
         assertThat(result2).isEqualTo(3400)
         // 아닌 경우에는 진입 자체가 x
     }
 
     @Test
-    fun `주말 할인 가격 확인`(){
+    fun `주말 할인 가격 확인`() {
         // 메인 메뉴 있을 때 할인
         val check = WeeklyEvent().weekendDay(mutableMapOf("티본스테이크" to 1, "레드와인" to 1))
         assertThat(check).isEqualTo(2023)
@@ -29,7 +29,7 @@ class EventsTest {
     }
 
     @Test
-    fun `평일 할인 가격 확인`(){
+    fun `평일 할인 가격 확인`() {
         // 디저트 메뉴 있을 때 할인
         val check = WeeklyEvent().weekDay(mutableMapOf("티본스테이크" to 1, "초코케이크" to 2))
         assertThat(check).isEqualTo(4046)
