d54fd2430fc9ff5c1f49c5a811f226ad1180a6ed
yujin37 <yj061628@gmail.com>
2023-11-14 15:39:58 +0900

    fix: 다른 함수들의 파라미터 값 변경에 따른 테스트 코드 변경

--- src/test/kotlin/christmas/EventBenefitCalculatorTest.kt
+++ src/test/kotlin/christmas/EventBenefitCalculatorTest.kt
@@ -7,19 +7,24 @@ import org.junit.jupiter.api.Test
 class EventBenefitCalculatorTest {
     @Test
     fun `주말 할인 적용 확인`() {
-        val check = EventBenefitCalculator().benefitDetails(29, 100000, mutableMapOf("해산물파스타" to 2, "초코케이크" to 2))
+        val params = EventParameters(29, 100000, mutableMapOf("해산물파스타" to 2, "초코케이크" to 2))
+        val check = EventBenefitCalculator(params).benefitDetails()
         Assertions.assertThat(check).isEqualTo(mutableMapOf("주말 할인" to 4046))
-        val check2 = EventBenefitCalculator().benefitDetails(2, 100000, mutableMapOf("해산물파스타" to 2, "초코케이크" to 2))
+        val params2 = EventParameters(2, 100000, mutableMapOf("해산물파스타" to 2, "초코케이크" to 2))
+        val check2 = EventBenefitCalculator(params2).benefitDetails()
         Assertions.assertThat(check2).isEqualTo(mutableMapOf("크리스마스 디데이 할인" to 1100, "주말 할인" to 4046))
     }
 
     @Test
     fun `평일 할인 적용 확인`() {
-        val check = EventBenefitCalculator().benefitDetails(28, 100000, mutableMapOf("해산물파스타" to 2, "초코케이크" to 2))
+        val params = EventParameters(28, 100000, mutableMapOf("해산물파스타" to 2, "초코케이크" to 2))
+        val check = EventBenefitCalculator(params).benefitDetails()
         Assertions.assertThat(check).isEqualTo(mutableMapOf("평일 할인" to 4046))
-        val check2 = EventBenefitCalculator().benefitDetails(24, 100000, mutableMapOf("해산물파스타" to 2, "초코케이크" to 2))
+        val params2 = EventParameters(24, 100000, mutableMapOf("해산물파스타" to 2, "초코케이크" to 2))
+        val check2 = EventBenefitCalculator(params2).benefitDetails()
         Assertions.assertThat(check2).isEqualTo(mutableMapOf("크리스마스 디데이 할인" to 3300, "평일 할인" to 4046, "특별 할인" to 1000))
-        val check3 = EventBenefitCalculator().benefitDetails(25, 100000, mutableMapOf("해산물파스타" to 2, "초코케이크" to 2))
+        val params3 = EventParameters(25, 100000, mutableMapOf("해산물파스타" to 2, "초코케이크" to 2))
+        val check3 = EventBenefitCalculator(params3).benefitDetails()
         Assertions.assertThat(check3).isEqualTo(mutableMapOf("크리스마스 디데이 할인" to 3400, "평일 할인" to 4046, "특별 할인" to 1000))
     }
 }
